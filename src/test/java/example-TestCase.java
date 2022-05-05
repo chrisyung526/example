89c8204d615e9e3e894fa5055f036e14254f2b6b
Chris Yung <56292367+chrisyung526@users.noreply.github.com>
2022-05-05 10:56:25 +0800

    Add files via upload
    test case

---
+++ src/test/java/example-TestCase.java
@@ -0,0 +1,14 @@
+package com.springhow.example.helloworld;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MyClassTest {
+	
+  @Test
+  public void testMethod() {
+    assertTrue(true);
+  }
+}
