ff0f257d796780bb2658718abb0a9d276ebbd1b5
Chris Yung <56292367+chrisyung526@users.noreply.github.com>
2022-05-05 15:44:00 +0800

    Update exampleTest.java

--- src/test/java/exampleTest.java
+++ src/test/java/exampleTest.java
@@ -5,9 +5,10 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.junit.runner.RunWith;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class exampleTest {
 	
