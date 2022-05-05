d93e454b6149f0f5be942a04ff70ab544c130979
Chris Yung <56292367+chrisyung526@users.noreply.github.com>
2022-05-05 12:44:45 +0800

    Update exampleTest.java

--- src/test/java/exampleTest.java
+++ src/test/java/exampleTest.java
@@ -4,19 +4,11 @@ import static org.junit.Assert.*;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = HelloWorldApplication.class)
 public class exampleTest {
 	
   @Test
   public void testMethod() {
     assertTrue(true);
-  }
-	
-  @Test
-  public void contextLoads() {
   }
 }
